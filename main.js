2a55e44ad58ae7b26aeb9e507dca9dff5c3ba506
feto <you@example.com>
2025-08-19 14:58:04 +0300

    lessssgoo

---
+++ main.js
@@ -0,0 +1,8 @@
+/* ===== JavaScript File =====
+   Currently empty.
+   You can add animations, dark/light mode, or
+   interactive features here later.
+*/
+
+// Example: Console greeting
+console.log("Welcome to Ahmed’s Blog 🚀");
